7c26b3f2dca8d7e8eb630b5b2a2c2f5a2d288f66
Les Leach <lleach1@swarthmore.edu>
2012-05-04 16:14:32 -0400

    Adding full site index creation

--- searchAutoComplete/autocomplete_indexer.php
+++ searchAutoComplete/autocomplete_indexer.php
@@ -3,63 +3,88 @@
 
 //where we are storing the json encoded index files
 $json_dir = '/srv/ingeniux-web/PreBuilt/scripts/json';
-
+$json_dir = './test_json';
 //directory for reading the Ingeniux XML files
 $xml_dir = '/srv/ingeniux-web';
+$xml_dir = './test_xml';
 
 $xpower_paths = array(
 	"its" => "/Content Store/Site Folder/Public Components/Home/Administration/ITS Home"
 );
 
-$xids = array(
-	"a-z_page" => array("x41451")
-);
-
 $dh = opendir($xml_dir);
-
-foreach ($xpower_paths AS $section=>$xpower_path){
-	$index_file = $json_dir . '/' . $section . '_index.js';
-	touch ($index_file) or die ("could not create/write to " . $index_file);
 	
-   	$index = array();
-    while (false !== ($file = readdir($dh))) {
-    	if (substr($file, -4) == ".xml"){
-        	$doc = new DOMDocument();
-			$doc->loadXML(file_get_contents($xml_dir . '/' . $file));
-			$doc_xpath = new DOMXpath($doc);
-			$rootnode = $doc_xpath->query('/*[1]');
-			if ($rootnode->length){
-				$xpowerattr = $rootnode->item(0)->attributes->getNamedItem('XPowerPath');
-				if ($xpowerattr){
+$index = array();
+
+while (false !== ($file = readdir($dh))) {
+	if (preg_match('/^x[0-9]+\.xml$/', $file)){
+		$doc = new DOMDocument();
+		$doc->loadXML(file_get_contents($xml_dir . '/' . $file));
+		$doc_xpath = new DOMXpath($doc);
+		$rootnode = $doc_xpath->query('/*[1]');
+		if ($rootnode->length){
+			$xpowerattr = $rootnode->item(0)->attributes->getNamedItem('XPowerPath');
+			$layoutattr = $rootnode->item(0)->attributes->getNamedItem('Layout');
+			if ($xpowerattr and $layoutattr){
+				$index_match = array('site');
+				foreach ($xpower_paths AS $section=>$xpower_path){ //add to xpowerpath collection if we have a match
 					if ($xpower_path == substr($xpowerattr->value, 0, strlen($xpower_path))){
-						$page_index = array('value' => $file);
-						$titlenode = $doc_xpath->query('/*/PageTitle');
-						if ($titlenode->length){
-							if (strlen(strip_tags(trim($titlenode->item(0)->nodeValue)))){
-								$page_index['label'] = $page_index['title'] = strip_tags(trim($titlenode->item(0)->nodeValue));
-								$keywordsnode = $doc_xpath->query('/*/MetaKeywords');
-								if ($keywordsnode->length){
-									if (strlen($keywordsnode->item(0)->nodeValue)){
-										$page_index['label'] = $keywordsnode->item(0)->nodeValue . ', ' . $page_index['label'];
-									}
-								}
-								$index[$xpowerattr->value] = $page_index;
+						$index_match[] = $section;
+					}
+				}
+				$page_index = array(
+					'value' => $file,
+					'url' => get_final_url($file)
+				);
+				$titlenode = $doc_xpath->query('/*/PageTitle');
+				if ($titlenode->length){
+					if (strlen(strip_tags(trim($titlenode->item(0)->nodeValue)))){
+						$page_index['label'] = $page_index['title'] = strip_tags(trim($titlenode->item(0)->nodeValue));
+						$keywordsnode = $doc_xpath->query('/*/MetaKeywords');
+						if ($keywordsnode->length){
+							if (strlen($keywordsnode->item(0)->nodeValue)){
+								$page_index['label'] = $keywordsnode->item(0)->nodeValue . ', ' . $page_index['label'];
 							}
 						}
-						
+						foreach ($index_match as $key=>$section){
+							$index[$section][$xpowerattr->value] = $page_index;
+						}
 					}
 				}
 			}
-        }
-    }
-	$index = array_values($index); //drop xpowerpath keys
-  	
-  	file_put_contents($index_file, 'jsonAutoCompData(' . my_json_encode($index) . ')');
-  	
-	rewinddir($dh);
-	
+		}
+	}
 }
+
 closedir($dh);
+
+foreach ($index as $section=>$pages){
+	$pages = array_values($pages); //drop xpowerpath keys
+	$index_file = $json_dir . '/' . $section . '_index.js';
+	touch ($index_file) or die ("could not create/write to " . $index_file);
+	file_put_contents($index_file, 'jsonAutoCompData(' . my_json_encode($pages) . ')');
+	//print 'jsonAutoCompData(' . my_json_encode($pages) . ')';
+}
+
+
+/** 
+ * Find the final (structured) URL when given an xID
+ */
+function get_final_url($xid){
+	$base_url = 'http://www.swarthmore.edu/';
+	$url = $base_url . $xid; 
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_exec ($ch);
+	return str_replace($base_url, '', curl_getinfo($ch, CURLINFO_EFFECTIVE_URL));
+}
 
 /**
  * turns php variables into JSON strings
